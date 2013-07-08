4788fd2679cd667b50918eceae4d84a87d66780c
Orlando Alemán Ortiz <contacto@orlandoaleman.com>
2013-07-08 13:08:48 +0100

    init

---
+++ Classes/TTPickerViewController.h
@@ -0,0 +1,12 @@
+//
+//  TTPickerViewController.h
+//
+//  Created by Orlando Aleman Ortiz on 08/07/13.
+//  Copyright (c) 2013 closett.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTPickerViewController : UITableViewController
+
+@end
